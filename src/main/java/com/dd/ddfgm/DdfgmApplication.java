4d3f08e3a02c0cd94fc808562f5e2f7033cf2908
jdk8_666 <yjdqw@126.com>
2020-05-07 22:22:50 +0800

    创建gmtool项目

---
+++ src/main/java/com/dd/ddfgm/DdfgmApplication.java
@@ -0,0 +1,13 @@
+package com.dd.ddfgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdfgmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdfgmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dd/ddfgm/DdfgmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dd.ddfgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdfgmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
